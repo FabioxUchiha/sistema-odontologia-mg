afc82ec0a36f92a3f298e4980e8299bd1290fd4f
FabioxUchiha <fcastellanos43@uan.edu.co>
2020-09-10 19:46:41 -0500

    cambios en boton crear inventarios

--- resources/views/ingresos/index.blade.php
+++ resources/views/ingresos/index.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1 class="pull-left">Ingresos</h1>
         <h1 class="pull-right">
-            @can('ver_inventarios')
+            @can('crear_inventarios')
            <a class="btn btn-info pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('ingresos.create') }}">Agregar Ingreso o Egreso</a>
             @endcan
         </h1>
--- resources/views/inventarios/index.blade.php
+++ resources/views/inventarios/index.blade.php
@@ -4,7 +4,11 @@
     <section class="content-header">
         <h1 class="pull-left">Inventarios</h1>
         <h1 class="pull-right">
-            @can('ver_inventarios')
+            @can('crear_inventarios')
+           {{-- exportar inventarios --}}
+           <a class="btn btn-info pull-right" style="margin-top: -10px;margin-bottom: 5px" href="#">Exportar a Excel</a>
+            @endcan
+            @can('crear_inventarios')
            <a class="btn btn-info pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('inventarios.create') }}">Agregar Nuevo Producto</a>
             @endcan
         </h1>
