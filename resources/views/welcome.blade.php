1f1ca984be56f6c2f71d417f0c009702a512a323
FabioxUchiha <fcastellanos43@uan.edu.co>
2020-09-12 14:39:49 -0500

    update js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,13 +132,13 @@
 </body>
 
 <!-- jQuery -->
-<script type="text/javascript" src="../resources/js/jquery.min.js"></script>
+<script type="text/javascript" src="{{ asset('/js/jquery.min.js') }}"></script>
 <!-- Bootstrap tooltips -->
-<script type="text/javascript" src="../resources/js/popper.min.js"></script>
+<script type="text/javascript" src="{{ asset('/js/popper.min.js') }}"></script>
 <!-- Bootstrap core JavaScript -->
-<script type="text/javascript" src="../resources/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="{{ asset('/js/bootstrap.min.js') }}"></script>
 <!-- MDB core JavaScript -->
-<script type="text/javascript" src="../resources/js/mdb.min.js"></script>
+<script type="text/javascript" src="{{ asset('/js/mdb.min.js') }}"></script>
 <!-- Your custom scripts (optional) -->
 <script type="text/javascript"></script>
 
