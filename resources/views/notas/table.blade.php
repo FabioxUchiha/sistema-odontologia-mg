30b0f35ea23add195230dc9809950b570ac35fc1
FabioxUchiha <fcastellanos43@uan.edu.co>
2020-10-11 13:54:52 -0500

    Update table.blade.php

--- resources/views/notas/table.blade.php
+++ resources/views/notas/table.blade.php
@@ -7,7 +7,7 @@
                 <th>Telefono</th>
                 <th>Procedimiento</th>
                 <th>Observaciones</th>
-                <th colspan="3">Acciones</th>
+                <th colspan="3" class="col-md-1">Acciones</th>
             </tr>
         </thead>
         <tbody>
