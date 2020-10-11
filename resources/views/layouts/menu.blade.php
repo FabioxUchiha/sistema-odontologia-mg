612be9fcff7becaa54e116045c731498216357e3
FabioxUchiha <fcastellanos43@uan.edu.co>
2020-10-10 23:51:20 -0500

    Update menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,26 +4,31 @@
     <a href="{{ route('users.index') }}"><i class="fa fa-user-circle"></i><span>Usuarios</span></a>
 </li>
 @endcan
+
 @canany(['ver roles', 'borrar roles', 'crear roles','editar roles'])
 <li class="{{ Request::is('roles*') ? 'active' : '' }}">
     <a href="{{ route('roles.index') }}"><i class="fa fa-address-card"></i><span>Roles</span></a>
 </li>
 @endcan
+
 @canany(['ver inventarios', 'borrar inventarios', 'crear inventarios','editar inventarios'])
 <li class="{{ Request::is('inventarios*') ? 'active' : '' }}">
     <a href="{{ route('inventarios.index') }}"><i class="fa fa-cubes"></i><span>Inventarios</span></a>
 </li>
 @endcan
+
 @canany(['ver ingresos', 'borrar ingresos', 'crear ingresos','editar ingresos'])
 <li class="{{ Request::is('ingresos*') ? 'active' : '' }}">
     <a href="{{ route('ingresos.index') }}"><i class="fa fa-money"></i><span>Ingresos y Egresos</span></a>
 </li>
 @endcan
+
 @canany(['ver insumos', 'borrar insumos', 'crear insumos','editar insumos'])
 <li class="{{ Request::is('insumos*') ? 'active' : '' }}">
     <a href="{{ route('insumos.index') }}"><i class="fa fa-archive"></i><span>Insumos</span></a>
 </li>
 @endcan
+
 @canany(['ver notas', 'borrar notas', 'crear notas','editar notas'])
 <li class="{{ Request::is('notas*') ? 'active' : '' }}">
     <a href="{{ route('notas.index') }}"><i class="fa fa-clipboard"></i><span>Notas</span></a>
@@ -34,41 +39,48 @@
     <a href="{{ route('dashboard.index') }}"><i class="fa fa-tachometer"></i><span>Dashboard</span></a>
 </li>
 {{-- @endcan --}}
+
+    {{-- @canany(['ver notas', 'borrar notas', 'crear notas','editar notas']) --}}
 <li>
-{{-- @canany(['ver notas', 'borrar notas', 'crear notas','editar notas']) --}}
-  <a class="dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-		<i class="glyphicon glyphicon-th-list"></i>
-    <span>
-    Desplegables
-    </span>
-    <span class="caret"></span>
-  </a>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+    <a class="dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+        <i class="glyphicon glyphicon-th-list"></i>
+          <span>
+            Desplegables
+            </span>
+        <span class="caret"></span>
+    </a>
+<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+
     <li class="{{ Request::is('desplegableNombreInventarios*') ? 'active' : '' }}">
-    <a class="text-black"
+        <a class="text-black"
         href="{{ route('desplegableNombreInventarios.index') }}">
         <span>Nombre Instrumental</span></a>
     </li>
-    <li role="separator" class="divider"></li>
+
     <li class="{{ Request::is('desplegableServicioIngresos*') ? 'active' : '' }}">
         <a class="text-black"
         href="{{ route('desplegableServicioIngresos.index') }}">
         <span>Nombre Servicios</span></a>
     </li>
-    <li role="separator" class="divider"></li>
-{{-- @endcan --}}
-  </ul>
+
+    <li class="{{ Request::is('desplegableCategoriaInstrumentals*') ? 'active' : '' }}">
+        <a class="text-black"
+        href="{{ route('desplegableCategoriaInstrumentals.index') }}">
+        <span>Desplegable Categoria Instrumentals</span></a>
+    </li>
+
+    <li class="{{ Request::is('desplegableMarcaInstrumentals*') ? 'active' : '' }}">
+        <a class="text-black"
+        href="{{ route('desplegableMarcaInstrumentals.index') }}">
+        <span>Desplegable Marca Instrumentals</span></a>
+    </li>
+</ul>
+    {{-- @endcan --}}
 
 </li>
 
 
 
 
-<li class="{{ Request::is('desplegableCategoriaInstrumentals*') ? 'active' : '' }}">
-    <a href="{{ route('desplegableCategoriaInstrumentals.index') }}"><i class="fa fa-edit"></i><span>Desplegable Categoria Instrumentals</span></a>
-</li>
 
-<li class="{{ Request::is('desplegableMarcaInstrumentals*') ? 'active' : '' }}">
-    <a href="{{ route('desplegableMarcaInstrumentals.index') }}"><i class="fa fa-edit"></i><span>Desplegable Marca Instrumentals</span></a>
-</li>
 
